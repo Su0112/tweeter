2cad038e0d674a6dbdab658b9b475ccecda281dc
SuShruthi <shruthacm@gmail.com>
2023-05-24 02:21:40 -0700

    fetch tweets with Ajax

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -41,44 +41,64 @@
 
 
 // Fake data taken from initial-tweets.json
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
+// const data = [
+//   {
+//     "user": {
+//       "name": "Newton",
+//       "avatars": "https://i.imgur.com/73hZDYK.png"
+//       ,
+//       "handle": "@SirIsaac"
+//     },
+//     "content": {
+//       "text": "If I have seen further it is by standing on the shoulders of giants"
+//     },
+//     "created_at": 1461116232227
+//   },
+//   {
+//     "user": {
+//       "name": "Descartes",
+//       "avatars": "https://i.imgur.com/nlhLi3I.png",
+//       "handle": "@rd"
+//     },
+//     "content": {
+//       "text": "Je pense , donc je suis"
+//     },
+//     "created_at": 1461113959088
+//   }
+// ];
 $(document).ready(function() {
   const $form = $("form");
+
+  const loadTweets = function() {
+    // Make a GET request to the server using Ajax
+    $.ajax({
+      url: "http://localhost:8080/tweets",
+      method: "GET",
+    }).then((res) => {
+      console.log("render Tweets: ", res);
+      // Call the renderTweets function and pass the response as an argument
+      renderTweets(res);
+    });
+  };
+
+  // Call the loadTweets function to initiate the GET request
+  loadTweets();
+
   // Add the event listener for form submission
   $form.submit((event) => {
     // Prevent the default form submission behavior
     event.preventDefault();
+
     // Serialize the form data into a query string
     const formData = $form.serialize();
+
     // Make a POST request to the server using Ajax
     $.ajax({
       url: "http://localhost:8080/tweets",
       method: "POST",
       data: formData,
+    }).then(() => {
+      loadTweets();
     });
     console.log(event);
     $form[0].reset();
@@ -118,4 +138,4 @@ const createTweetElement = function(tweet) {
   return $tweet;
 };
 
-renderTweets(data);
+//renderTweets(data);
